bb0f2536d4e2d5d4b9c522afd123c87a57f0e857
SakshiSambhaji24 <sakshiksambhaji24@gmail.com>
2025-11-06 19:06:13 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,13 @@
+add = lambda a, b: a + b
+
+multiply = lambda a, b: a * b
+
+square = lambda x: x ** 2
+
+cube = lambda x: x ** 3
+
+x, y = 5, 3
+print("Addition:", add(x, y))
+print("Multiplication:", multiply(x, y))
+print("Square of", x, ":", square(x))
+print("Cube of", y, ":", cube(y))
